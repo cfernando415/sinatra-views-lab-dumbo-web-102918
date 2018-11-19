70b42ed33e3227df411bb5f749de2cfd7245b99c
cfernando415 <ca.fernando415@gmail.com>
2018-11-19 14:57:30 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 class App < Sinatra::Base
-
-	get '/' do
-		erb :index
+	configure do
+		set :views, "views"
+		set :public_dir, "public"
 	end
-
-
+	get '/hello' do
+		erb :hello
+	end
+	get '/goodbye' do
+		erb :goodbye
+	end
+	get '/date' do
+		erb :date
+	end
 end
